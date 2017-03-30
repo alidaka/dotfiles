85621b1d3eeb6b6bdfce8167833354b45bc37774
alidaka <alidaka@gmail.com>
2017-03-29 23:17:30 -0400

    fix shebang; shellcheck; don't bother with absolute executable paths

--- ssh/lock.sh
+++ ssh/lock.sh
@@ -1,17 +1,17 @@
-#/bin/sh
+#!/bin/sh
 set -e
 
 SOURCE=${1%/}
-if [[ -z $SOURCE ]]; then
-  /bin/echo "Usage: ${0} <source_directory> [output_file]"
+if [ -z "$SOURCE" ]; then
+  echo "Usage: ${0} <source_directory> [output_file]"
   exit 1
 fi
 
 DEST=${2}
-if [[ -z $DEST ]]; then
+if [ -z "$DEST" ]; then
   DEST=$SOURCE.gpg
 fi
 
-GPG_TTY=$(tty) /usr/bin/tar c $SOURCE | /usr/local/bin/gpg --symmetric --cipher-algo AES256 --output $DEST
+GPG_TTY=$(tty) tar c "$SOURCE" | gpg --symmetric --cipher-algo AES256 --output "$DEST"
 
-/bin/echo $DEST
+echo "$DEST"

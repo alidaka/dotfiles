2114a6ed26fed96c1638ed9320ba81a95272d7a3
alidaka <alidaka@gmail.com>
2017-03-29 23:22:32 -0400

    shellcheck; tar option order

--- ssh/unlock.sh
+++ ssh/unlock.sh
@@ -1,15 +1,15 @@
-#/bin/sh
+#!/bin/sh
 set -e
 
 SOURCE=$1
-if [[ -z $SOURCE ]]; then
-  /bin/echo "Usage: ${0} <source_file.tar.gpg> [destination_directory]"
+if [ -z "$SOURCE" ]; then
+  echo "Usage: ${0} <source_file.tar.gpg> [destination_directory]"
   exit 1
 fi
 
 DEST=${2}
-if [[ -z $DEST ]]; then
+if [ -z "$DEST" ]; then
   DEST=${PWD}
 fi
 
-/usr/local/bin/gpg --decrypt $SOURCE | tar -C $DEST xf -
+gpg --decrypt "$SOURCE" | tar xf - -C "$DEST"

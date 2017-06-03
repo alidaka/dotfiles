ac290512a0490864c74d57ad32663e37c92c126e
alidaka <alidaka@gmail.com>
2017-06-03 19:00:46 -0400

    Linux implementation

--- ssh/add.sh
+++ ssh/add.sh
@@ -7,25 +7,32 @@ if [ -z "$MINUTES" ]; then
 fi
 
 # Mount and decrypt
+SCRIPT_DIR=$(dirname "$0")
 if [ "$OS" = "Darwin" ]; then
   # size in 512b sectors
   RAMDISK=$(hdiutil attach -nomount ram://$((2 * 1024 * 5)))
   diskutil eraseVolume HFS+ ramdisk "$RAMDISK"
-else
-  echo "linux...WIP"
-  exit 1
-fi
 
-# Add key
-ssh-add -D
-ssh-add -t ${MINUTES}M
+  ssh-add -D
+  ssh-add -t ${MINUTES}M
 
-# Unmount
-if [ "$OS" = "Darwin" ]; then
   echo "WIP"
   # umount -f ...
   # hdiutil detach ...
 else
-  echo "linux...WIP"
-  exit 1
+  if [ ! -d /dev/shm ]; then
+    echo "$0 needs the /dev/shm memory-backed directory, but it seems to be missing :("
+    exit 1
+  fi
+
+  TEMP_DIR=$(mktemp --directory --tmpdir=/dev/shm)
+  "$SCRIPT_DIR/scripts/unlock.sh" "$SCRIPT_DIR/.ssh.gpg" "$TEMP_DIR"
+
+  if [ -z "$SSH_AGENT_PID" ]; then
+    eval $(ssh-agent -s)
+  fi
+  ssh-add -D
+  ssh-add -t ${MINUTES}M "$TEMP_DIR/id_rsa"
+
+  rm --recursive --force "$TEMP_DIR"
 fi

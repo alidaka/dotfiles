a4b506889dcb74ea765dec65aa58aec1baebb9ff
alidaka <alidaka@gmail.com>
2017-06-03 20:01:52 -0400

    make user start ssh-agent; clean up notes

--- ssh/add.sh
+++ ssh/add.sh
@@ -6,11 +6,20 @@ if [ -z "$MINUTES" ]; then
   MINUTES=1
 fi
 
+if [ -z "$SSH_AGENT_PID" ]; then
+  echo "Please start the ssh-agent, e.g.:"
+  echo
+  echo "    eval \$(ssh-agent -s)"
+  echo
+  exit 1
+fi
+
 # Mount and decrypt
 SCRIPT_DIR=$(dirname "$0")
 if [ "$OS" = "Darwin" ]; then
   # size in 512b sectors
-  RAMDISK=$(hdiutil attach -nomount ram://$((2 * 1024 * 5)))
+  SIZE_IN_MB=5 # TODO: untested
+  RAMDISK=$(hdiutil attach -nomount ram://$((2 * 1024 * SIZE_IN_MB)))
   diskutil eraseVolume HFS+ ramdisk "$RAMDISK"
 
   ssh-add -D
@@ -28,9 +37,6 @@ else
   TEMP_DIR=$(mktemp --directory --tmpdir=/dev/shm)
   sh "$SCRIPT_DIR/scripts/unlock.sh" "$SCRIPT_DIR/.ssh.gpg" "$TEMP_DIR"
 
-  if [ -z "$SSH_AGENT_PID" ]; then
-    eval $(ssh-agent -s)
-  fi
   ssh-add -D
   ssh-add -t ${MINUTES}M "$TEMP_DIR/id_rsa"
 

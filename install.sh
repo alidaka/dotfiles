803cb9c3a1b79f797eb9c46f63a5334ba871bf96
alidaka <alidaka@gmail.com>
2018-12-09 07:59:04 +0000

    install.sh: add messages, install a ruby

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -e
 
+echo "Install packages..."
 apt install curl vim
+echo "Done!"
 
+echo "Install vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Done!"
+
+echo "Install rvm/ruby..."
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
+echo "Done!"

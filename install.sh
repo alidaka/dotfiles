631f97cf4ce836fcee1e3d89899ef53b3a3dbe17
alidaka <alidaka@gmail.com>
2018-12-15 12:35:50 -0800

    install.sh: darwin, and some idempotence

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
 #!/bin/bash -e
 
 echo "Install packages..."
-apt install curl vim
+PACKAGES="curl vim zsh gpg"
+if [ $(uname -s) = "Darwin" ]; then
+  brew install $PACKAGES
+else
+  apt install $PACKAGES
+fi
 echo "Done!"
 
-echo "Install vim-plug..."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "Done!"
+#echo "Install vim-plug..."
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#vim +PlugInstall +qall
+#echo "Done!"
+
+echo "Install oh-my-zsh..."
+if command -v zsh 2>/dev/null; then
+  echo "Already installed; skipped!"
+else
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "Done!"
+fi
 
 echo "Install rvm/ruby..."
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /usr/local/rvm/scripts/rvm
-echo "Done!"
+if command -v rvm 2>/dev/null; then
+  echo "Already installed; skipped!"
+else
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby
+  source /usr/local/rvm/scripts/rvm
+  echo "Done!"
+fi

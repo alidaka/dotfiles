48c0582d108fd344d3063e8b7866f13f460d37a5
alidaka <alidaka@gmail.com>
2021-03-04 17:35:02 -0800

    more Mac defaults

--- mac-config/shelf.sh
+++ mac-config/shelf.sh
@@ -5,5 +5,12 @@ brew cask install scroll-reverser
 brew install tmux
 brew install tree
 brew install ripgrep
+brew install jq
 
 "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --install-extension vscodevim.vim
+
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.apple.Finder AppleShowAllFiles -bool true; killall Finder
+
+brew cask install gifcapture

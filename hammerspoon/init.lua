af54d1bff1be34fbd81c1fa171337330dc32674e
Augustus Lidaka <augustus@shelfengine.com>
2022-04-14 16:47:31 -0700

    hammerspoon: fix indentation

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -53,27 +53,27 @@ mouseCircle = nil
 mouseCircleTimer = nil
 
 function mouseHighlight()
-    -- Delete an existing highlight if it exists
-    if mouseCircle then
-        mouseCircle:delete()
-        if mouseCircleTimer then
-            mouseCircleTimer:stop()
-        end
+  -- Delete an existing highlight if it exists
+  if mouseCircle then
+    mouseCircle:delete()
+    if mouseCircleTimer then
+      mouseCircleTimer:stop()
     end
-    -- Get the current co-ordinates of the mouse pointer
-    mousepoint = hs.mouse.absolutePosition()
-    -- Prepare a big red circle around the mouse pointer
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
-    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
-    mouseCircle:setFill(false)
-    mouseCircle:setStrokeWidth(5)
-    mouseCircle:show()
+  end
+  -- Get the current co-ordinates of the mouse pointer
+  mousepoint = hs.mouse.absolutePosition()
+  -- Prepare a big red circle around the mouse pointer
+  mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+  mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+  mouseCircle:setFill(false)
+  mouseCircle:setStrokeWidth(5)
+  mouseCircle:show()
 
-    -- Set a timer to delete the circle
-    mouseCircleTimer = hs.timer.doAfter(1, function()
-      mouseCircle:delete()
-      mouseCircle = nil
-    end)
+  -- Set a timer to delete the circle
+  mouseCircleTimer = hs.timer.doAfter(1, function()
+    mouseCircle:delete()
+    mouseCircle = nil
+  end)
 end
 hs.hotkey.bind(mash, "D", mouseHighlight)
 

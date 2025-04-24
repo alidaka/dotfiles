7877f0a582abc5184367a1dbb8c25719e9ea6df7
alidaka <augustus.lidaka@calicoenergy.com>
2025-04-23 18:14:44 -0700

    runme.sh: reorganize for clarity

--- runme.sh
+++ runme.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 source_dir=$( cd $(dirname $0) ; pwd -P )
-backup=~/dotfiles_backup
 files="vimrc gitconfig tmux.conf zshrc hammerspoon/init.lua"
 
+# Prep for backup (if needed)
+backup=~/dotfiles_backup
 if [ -d $backup ]; then
   echo "Dotfiles backup directory $backup already exists--aborting!"
   exit 1
@@ -14,15 +15,20 @@ else
   echo "Backing up existing dotfiles to $backup..."
 fi
 
+backup() {
+  if [ -f $1 ] || [ -h $1 ]; then
+    echo "  Backing up $1 to $backup..."
+    mkdir -p $backup
+    mv $1 $backup
+  fi
+}
+
+# Most of the files
 for file in $files; do
   dest=~/.$file
   echo "$file --> $dest"
 
-  if [ -f $dest ] || [ -h $dest ]; then
-    echo "  Backing up $dest to $backup..."
-    mkdir -p $backup
-    mv $dest $backup
-  fi
+  backup $dest
 
   echo "  Creating link for $file..."
   ln -s $source_dir/$file $dest
